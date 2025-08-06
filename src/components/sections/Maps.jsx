06c8d5e2efa77050c323a00bfdd18644b5973d54
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-06 14:51:10 -0300

    fix: Seção Maps adicionada

--- src/components/sections/Maps.jsx
+++ src/components/sections/Maps.jsx
@@ -36,7 +36,7 @@ export default function Maps({ colorMode }) {
   const titleColor = textClasses[colorMode] || textClasses.default;
 
   return (
-    <SectionArea className={`${bgClass}`}>
+    <SectionArea className={`${bgClass}`} id="maps">
       <SectionWrapper className="flex flex-col desktop1:flex-row gap-[40px] desktop2:gap-0 desktop1:justify-between desktop1:items-start">
         <MotionDivDownToUp className="flex flex-col justify-center w-full">
           <SectionHeader
