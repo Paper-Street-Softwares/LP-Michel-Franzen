5a234bb6c19c2e5701dc7bce722f24a4cda7d8fe
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-06 15:00:15 -0300

    fix: Adiciona link Maps na Sidebar

--- src/components/sectionElements/SidebarSocial.jsx
+++ src/components/sectionElements/SidebarSocial.jsx
@@ -9,6 +9,7 @@ import {
   ServerIcon,
   UserSearch,
   AlignJustify,
+  MapPin,
 } from "lucide-react";
 import { Link } from "react-scroll";
 import content from "../../content/content";
@@ -180,6 +181,26 @@ export default function SidebarSocial({ colorMode }) {
 
                             <li>
                               <a className="flex items-center w-full p-3 transition-colors cursor-pointer p-ripple border-round text-700 hover:surface-100 transition-duration-150">
+                                <MapPin />
+                                <span className="ml-[8px]">
+                                  <Link
+                                    to="maps"
+                                    className="align-text-top cursor-pointer"
+                                    spy={true}
+                                    smooth={true}
+                                    duration={500}
+                                    offset={-70}
+                                    href="#"
+                                  >
+                                    {content.texts.navbar.menuItems[3]}
+                                  </Link>
+                                </span>
+                                <Ripple />
+                              </a>
+                            </li>
+
+                            <li>
+                              <a className="flex items-center w-full p-3 transition-colors cursor-pointer p-ripple border-round text-700 hover:surface-100 transition-duration-150">
                                 <HelpCircle />
                                 <span className="ml-[8px]">
                                   <Link
@@ -191,7 +212,7 @@ export default function SidebarSocial({ colorMode }) {
                                     offset={-70}
                                     href="#"
                                   >
-                                    {content.texts.navbar.menuItems[3]}
+                                    {content.texts.navbar.menuItems[4]}
                                   </Link>
                                 </span>
                                 <Ripple />
